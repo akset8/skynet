b5891f8d9ab4da478e5530b95aca445430bb2c48
System Administrator <root@MacBook-2.local>
2016-07-28 03:58:47 +0530

    changes to flasky

--- app.py
+++ app.py
@@ -57,10 +57,6 @@ def uploaded_file(filename):
                                filename)
 
 if __name__ == '__main__':
-    app.run(
-        host="0.0.0.0",
-        port=int("80"),
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=int("80"))
 
 
